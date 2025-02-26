97e660a6c708bbfc9aa7c628809bca42f1549e83
kyungwon-kang <kkw.theo@gmail.com>
2025-02-26 14:32:44 +0900

    제네릭 적용

---
+++ src/generic/ex1/BoxMain3.java
@@ -0,0 +1,25 @@
+package generic.ex1;
+
+public class BoxMain3 {
+    public static void main(String[] args) {
+        GenericBox<Integer> integerBox = new GenericBox<>();
+        integerBox.set(10);
+        Integer integer = integerBox.get();
+        System.out.println("integer = " + integer);
+
+        GenericBox<String> stringBox = new GenericBox<>();
+        stringBox.set("Hello");
+        String str = stringBox.get();
+        System.out.println("str = " + str);
+
+        GenericBox<Double> doubleBox = new GenericBox<>();
+        doubleBox.set(10.5);
+        Double doubleValue = doubleBox.get();
+        System.out.println("doubleValue = " + doubleValue);
+
+
+        GenericBox<Integer> integerBox2 = new GenericBox<>();
+
+
+    }
+}
