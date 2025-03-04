af85867e31cd1563874b92d41ed4bf9e8eed2f6e
Theo <kkw.theo@gmail.com>
2025-03-04 12:27:37 +0900

    add: 제네릭 용어와 관례

---
+++ src/generic/ex1/RowBoxMain.java
@@ -0,0 +1,12 @@
+package generic.ex1;
+
+public class RowBoxMain {
+    public static void main(String[] args) {
+        GenericBox integerBox = new GenericBox();
+//        GenericBox<Object> integerBox = new GenericBox<>(); // 권장
+
+        integerBox.set(10);
+        Integer integer = (Integer) integerBox.get();
+        System.out.println("integer = " + integer);
+    }
+}
