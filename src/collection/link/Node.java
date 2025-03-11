5ad3daaefdbb1ffef2221e095359cf81cfae31bf
Theo <kkw.theo@gmail.com>
2025-03-11 20:53:28 +0900

    add : 노드와 연결2

--- src/collection/link/Node.java
+++ src/collection/link/Node.java
@@ -8,5 +8,27 @@ public class Node {
         this.item = item;
     }
 
+//    @Override
+//    public String toString() {
+//        return "Node{" +
+//                "item=" + item +
+//                ", next=" + next +
+//                '}';
+//    }
 
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        Node x = this;
+        sb.append("[");
+        while (x != null) {
+            sb.append(x.item);
+            if(x.next != null) {
+                sb.append("->");
+            }
+            x = x.next;
+        }
+        sb.append("]");
+        return sb.toString();
+    }
 }
