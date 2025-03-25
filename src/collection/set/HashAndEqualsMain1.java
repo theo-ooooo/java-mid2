f44a16931613f7fda429acadfe22e1be2dbd873d
Theo <kkw.theo@gmail.com>
2025-03-25 23:33:47 +0900

    add: equals, hashCode 의 중요성1

---
+++ src/collection/set/HashAndEqualsMain1.java
@@ -0,0 +1,27 @@
+package collection.set;
+
+import collection.set.member.MemberNoHashNoEq;
+
+public class HashAndEqualsMain1 {
+    public static void main(String[] args) {
+
+        //중복 등록
+        MyHashSetV2 set = new MyHashSetV2(10);
+        MemberNoHashNoEq m1 = new MemberNoHashNoEq("A");
+        MemberNoHashNoEq m2 = new MemberNoHashNoEq("A");
+        System.out.println("m1.hashCode() = " + m1.hashCode());
+        System.out.println("m2.hashCode() = " + m2.hashCode());
+        System.out.println("m1.equals(m2) = " + m1.equals(m2));
+
+        set.add(m1);
+        set.add(m2);
+        System.out.println(set);
+
+
+        // 검색 실패
+        MemberNoHashNoEq searchValue = new MemberNoHashNoEq("A");
+        System.out.println("searchValue.hashCode() = " + searchValue.hashCode());
+        boolean result = set.contains(searchValue);
+        System.out.println("result = " + result);
+    }
+}
