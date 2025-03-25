c37fac4762d2974ef46d1844e5a0c16f2df3c5ae
Theo <kkw.theo@gmail.com>
2025-03-25 14:11:20 +0900

    add: 직접 구현하는 Set0 - 시작

---
+++ src/collection/set/MyHashSetV0Main.java
@@ -0,0 +1,19 @@
+package collection.set;
+
+public class MyHashSetV0Main {
+    public static void main(String[] args) {
+        MyHashSetV0 set = new MyHashSetV0();
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        set.add(4);
+        System.out.println("set = " + set);
+
+        boolean result = set.add(4);
+        System.out.println("result = " + result);
+        System.out.println("set = " + set);
+
+        System.out.println("set.contains(3) = " + set.contains(3));
+        System.out.println("set.contains(99) = " + set.contains(99));
+    }
+}
