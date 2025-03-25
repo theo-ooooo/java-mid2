72b64ff7f9615a455c98f16918f09aa26569eb7c
Theo <kkw.theo@gmail.com>
2025-03-25 16:50:10 +0900

    add: 문자열 해시 코드

---
+++ src/collection/set/StringHashMain.java
@@ -0,0 +1,31 @@
+package collection.set;
+
+public class StringHashMain {
+    static final int CAPACITY = 10;
+
+    public static void main(String[] args) {
+        //char
+        char charA = 'A';
+        char charB = 'B';
+        System.out.println(charA + " = " + (int)charA);
+        System.out.println(charB + " = " + (int)charB);
+        //hashCode
+
+        System.out.println("hashCode(\"A\") = " + hashCode("A"));
+        System.out.println("hashCode(\"B\") = " + hashCode("B"));
+        System.out.println("hashCode(\"AB\") = " + hashCode("AB"));
+    }
+
+    private static int hashCode(String str) {
+        char[] charArray = str.toCharArray();
+        int sum = 0;
+        for (char c : charArray) {
+            sum += c;
+        }
+        return sum;
+    }
+
+    private static int hashIndex(int value) {
+        return value % CAPACITY;
+    }
+}
