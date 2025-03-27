47c3d3f80e85182a5e576d3fea3d2153a6763d1c
Theo <kkw.theo@gmail.com>
2025-03-27 13:53:43 +0900

    feat: 컬렉션 유틸

---
+++ src/collection/utils/OfMain.java
@@ -0,0 +1,19 @@
+package collection.utils;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class OfMain {
+    public static void main(String[] args) {
+        //편리한 불변 컬렉션 생성 add 불가.
+        List<Integer> list = List.of(1, 2, 3);
+        Set<Integer> set = Set.of(1, 2, 3);
+        Map<Integer, String> map = Map.of(1, "one", 2, "two", 3, "three");
+
+        System.out.println("list = " + list);
+        System.out.println("set = " + set);
+        System.out.println("map = " + map);
+        System.out.println("list.getClass() = " + list.getClass());
+    }
+}
